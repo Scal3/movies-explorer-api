e0d3d2534d72dbb52136ce78c907565fc74710cb
mac <mac@dungeonMaster.local>
2021-11-12 22:45:39 +0300

    test-corse2

--- app.js
+++ app.js
@@ -42,6 +42,21 @@ app.use((req, res, next) => {
   next();
 });
 
+// Сложные cors запросы
+app.use((req, res, next) => {
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+
+  next();
+});
+
 // Роуты регистрации и логина
 app.use(regAndLogRouter);
 
